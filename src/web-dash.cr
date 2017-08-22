d945dd1192aa7a012c8af75ae45c3b0f46b2e860
Cyan101 <crazyandroid101@gmail.com>
2017-08-22 14:11:24 +0800

    Added reading the free/avail/total mem

--- src/web-dash.cr
+++ src/web-dash.cr
@@ -4,8 +4,34 @@ require "kemal-basic-auth"
 
 basic_auth "cyan101", "pass123"
 
+before_all do |env|
+  env.kemal_authorized_username? # Halt if fails
+end
+
+before_get do |env|
+  mem_info = `cat /proc/meminfo`.gsub(/(\ |(kb))/i, "").split("\n")
+  env.set("free_mem", free_mem(mem_info))
+  env.set("avail_mem", avail_mem(mem_info))
+  env.set("total_mem", total_mem(mem_info))
+end
+
 get "/" do |env|
-  "Hi, %s!" % env.kemal_authorized_username?
+  page = ""
+  page += "#{env.get("free_mem").as(String).to_i / 1024}MB Free<br>"
+  page += "#{env.get("avail_mem").as(String).to_i / 1024}MB Available<br>"
+  page += "#{env.get("total_mem").as(String).to_i / 1024}MB Total<br>"
 end
 
 Kemal.run
+
+def free_mem(mem)
+  mem[1].split(":").[1]
+end
+
+def avail_mem(mem)
+  mem[2].split(":").[1]
+end
+
+def total_mem(mem)
+  mem[0].split(":").[1]
+end

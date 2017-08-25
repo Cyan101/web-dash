18bbe4e4b9709627516aef88f8cad24555c5f236
Cyan/Jos <crazyandroid101@gmail.com>
2017-08-25 15:44:29 +0800

    Worked on reading the storage and displaying it on the dash

--- src/web-dash/storage.cr
+++ src/web-dash/storage.cr
@@ -1,27 +1,31 @@
-devices = [] of String
-devices << "/media/removable/Alter/"
-devices << "/var/host/media/removable/Alter"
-devices << "/"
-# Move above to a Config
+# Move above devices to a Config
 
-df = `df | tr -s [:space:]`.split("\n")
-devices_info = [] of String
-begin
-  df.each do |x|
-    mount_location = x.split(" ")[5]
-    devices_info << x if devices.includes?(mount_location)
+def get_storage
+  devices = [] of String
+  devices << "/media/removable/Alter/"
+  devices << "/var/host/media/removable/Alter"
+  devices << "/"
+
+  df = `df`.split("\n")
+  devices_info = [] of String
+  begin
+    df.each do |x|
+      mount_location = x.split(/\s{1,}/)[5]
+      devices_info << x if devices.includes?(mount_location)
+    end
+  rescue
+    puts "Error reading some devices"
   end
-rescue
-  puts "Error reading some devices"
+  return devices_info
 end
 
 # Filesystem 1K-blocks Used Available Use% Mounted-on
-devices_info.each do |x|
-  x = x.split(" ")
-  puts "#{x[5]} is #{x[4]} (#{kb_to_size(x[2].to_f)}) used"
-  puts "Filesystem: #{x[0]}"
-  puts "Total Space: #{kb_to_size(x[2].to_f + x[3].to_f)}"
-end
+# devices_info.each do |x|
+#  x = x.split(/\s{1,}/)
+#  puts "#{x[5]} is #{x[4]} (#{kb_to_size(x[2].to_f)}) used"
+#  puts "Filesystem: #{x[0]}"
+#  puts "Total Space: #{kb_to_size(x[2].to_f + x[3].to_f)}"
+# end
 
 def kb_to_size(size : Float | Int, round : Int? = 2)
   key = 0

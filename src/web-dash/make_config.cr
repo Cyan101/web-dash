9037e0bfb489dc0681f939603d30255c7760b254
Cyan/Jos <crazyandroid101@gmail.com>
2017-08-25 19:58:22 +0800

    Added "Program" Buttons to the Dash and Config

--- src/web-dash/make_config.cr
+++ src/web-dash/make_config.cr
@@ -1,5 +1,6 @@
 unless File.exists?("config.yaml")
   puts "No config found, creating config.yaml"
-  hash_cfg = {"background" => "http://i.imgur.com/ea9PB3H.png", "title" => "Web-Dash", "favicon" => "images/favicon.png"}
+  hash_cfg = {"background" => "http://i.imgur.com/ea9PB3H.png", "title" => "Web-Dash", "favicon" => "images/favicon.png",
+              "plex" => ":32400/web", "sonarr" => ":8989/", "radarr" => ":7878/", "transmission" => ":9091/"}
   File.write("config.yaml", hash_cfg.to_yaml)
 end

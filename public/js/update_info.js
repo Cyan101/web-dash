1d105e5f7d52ddb75e5f8d5a16efc3f9f22c43a0
Cyan/Jos <crazyandroid101@gmail.com>
2017-08-27 23:52:16 +0800

    Beautified to 2 Spaces

--- public/js/update_info.js
+++ public/js/update_info.js
@@ -1,22 +1,22 @@
 function update_stat(type){
-var req = new XMLHttpRequest();
-req.open('GET', '/update/' + type, false);   
-req.send(null);  
-if(req.status == 200) {  
-   var info = req.responseText;
-   document.getElementById(type).innerHTML = info;
-   }
+  var req = new XMLHttpRequest();
+  req.open('GET', '/update/' + type, false);
+  req.send(null);
+  if(req.status == 200) {
+    var info = req.responseText;
+    document.getElementById(type).innerHTML = info;
+  }
 }
 
 function update_bar(type){
-var req = new XMLHttpRequest();
-req.open('GET', '/update/bar-' + type, false);   
-req.send(null);  
-if(req.status == 200) {  
-   var info = req.responseText;
-   var bar = document.getElementById("bar-" + type);
-   bar.style.width = info;
-   }
+  var req = new XMLHttpRequest();
+  req.open('GET', '/update/bar-' + type, false);
+  req.send(null);
+  if(req.status == 200) {
+    var info = req.responseText;
+    var bar = document.getElementById("bar-" + type);
+    bar.style.width = info;
+  }
 }
 
 function update_page(){
@@ -25,6 +25,6 @@ function update_page(){
   update_stat("top");
   update_stat("memory");
   update_stat("cpu");
- }
+}
 
 setInterval(update_page, 4000);
